b9f4d84177253505f58d20954e867251ed7f17ff
Kirill Lobko <lobko.messi4@gmail.com>
2020-03-04 18:30:14 +0300

    feat: new part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (Array.isArray(array)==true && array.length!=0) ? array.sort(function(a, b) { return a - b; })[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (Array.isArray(array)==true && array.length!=0) ? array.sort(function(a, b) { return a - b; })[array.length-1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (Array.isArray(array)==true && array.length!=0) ? array.reduce((sum, current) => sum + current)/array.length : 0;
 }
